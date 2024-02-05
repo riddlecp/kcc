43123673422ef51f483b50d9e44eb6b3341c26f8
riddlecp <riddle.cp@gmail.com>
2024-02-05 09:06:21 -0500

    Script kindlegen get

---
+++ getKindlegen.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ECHO "Downloading Kindlegen"
+wget https://archive.org/download/kindlegen_linux_2_6_i386_v2_9/kindlegen_linux_2.6_i386_v2_9.tar.gz 
+
+ECHO "Extracting Kindlegen binary to /usr/local/bin/"
+tar -xzf kindlegen_linux_2.6_i386_v2_9.tar.gz -C /usr/local/bin kindlegen
+
+ECHO "Removing Kindlegen tar file"
+rm kindlegen_linux_2.6_i386_v2_9.tar.gz
