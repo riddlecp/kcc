76693ae8615818b4f781b12efcc1a2eb33c6854d
riddlecp <riddle.cp@gmail.com>
2024-02-05 09:11:15 -0500

    kindlegen script changes

--- getKindlegen.sh
+++ getKindlegen.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+ls -al
+
 ECHO "Downloading Kindlegen"
 wget https://archive.org/download/kindlegen_linux_2_6_i386_v2_9/kindlegen_linux_2.6_i386_v2_9.tar.gz 
 
 ECHO "Extracting Kindlegen binary to /usr/local/bin/"
-tar -xzf kindlegen_linux_2.6_i386_v2_9.tar.gz -C /usr/local/bin kindlegen
+tar xzf kindlegen_linux_2.6_i386_v2_9.tar.gz -C /usr/local/bin kindlegen
 
 ECHO "Removing Kindlegen tar file"
 rm kindlegen_linux_2.6_i386_v2_9.tar.gz
