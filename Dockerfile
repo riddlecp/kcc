42562649cb87818bf8963a40e9886de0a79a5e0a
riddlecp <riddle.cp@gmail.com>
2024-02-05 09:18:06 -0500

    logic updates

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,7 @@ LABEL org.opencontainers.image.licenses='ISC'
 LABEL org.opencontainers.image.title="Kindle Comic Converter"
 
 COPY . /opt/kcc
+RUN chmod +x /opt/kcc/getKindlegen.sh
 RUN /opt/kcc/getKindlegen.sh
 RUN cat /opt/kcc/kindlecomicconverter/__init__.py | grep version | awk '{print $3}' | sed "s/'//g" > /IMAGE_VERSION
 
--- getKindlegen.sh
+++ getKindlegen.sh
@@ -8,5 +8,8 @@ wget https://archive.org/download/kindlegen_linux_2_6_i386_v2_9/kindlegen_linux_
 ECHO "Extracting Kindlegen binary to /usr/local/bin/"
 tar xzf kindlegen_linux_2.6_i386_v2_9.tar.gz -C /usr/local/bin kindlegen
 
+ECHO "Making executable"
+chmod +x /usr/local/bin/kindlegen
+
 ECHO "Removing Kindlegen tar file"
 rm kindlegen_linux_2.6_i386_v2_9.tar.gz
